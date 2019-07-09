61f2992e3964edd43b3f06e0de62c0d6e8df7e01
Yangye Zhu <yangye.zhu@wishlife.com>
2019-07-09 16:20:45 -0700

    update pulsar-client to 2.4.0

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.pulsar/pulsar-client "2.1.1-incubating"]
+                 [org.apache.pulsar/pulsar-client "2.4.0"]
                  [cheshire "5.8.1"]]
   :main simple-pulsar.core)
