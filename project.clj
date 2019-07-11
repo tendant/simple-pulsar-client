20e282bac47dc0cfa4af53a561e5dc7fe997bad3
Yangye Zhu <yangye.zhu@wishlife.com>
2019-07-11 15:35:02 -0700

    update to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject simple-pulsar-client "0.1.1"
+(defproject simple-pulsar-client "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
